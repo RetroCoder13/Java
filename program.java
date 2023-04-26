864f43636373964e7452aa077490d7cb1260861b
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-04-26 10:54:18 +0000

    Workning program in Java

---
+++ program.java
@@ -0,0 +1,9 @@
+public class program{
+    public static void main(String[] args){
+        int i = 0;
+        while(i<5){
+            System.out.println("Looping");
+            i+=1;
+        }
+    }
+}
