576a9efbd29f15ad4537badc502c7bddb666f4e2
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-05-03 11:22:28 +0000

    Updated to use System.console()

--- program2.java
+++ program2.java
@@ -1,16 +1,11 @@
-import java.util.Scanner;
-
 public class program2 {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your password:");
-        String password = scanner.next();
+        String password = System.console().readLine();
         String currentPassword = "apple";
 
         if(password.equals(currentPassword)){
             System.out.println("Your password is: " + password);
         }
-
-        scanner.close();
     }
 }
