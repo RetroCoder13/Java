1bdb74c486bcd01d6ca7d902c01a74efbaa5a4bc
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-05-03 11:15:00 +0000

    ArrayList used in these programs. arrays3.java needs arguments to work

---
+++ arrays2.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class arrays2 {
+    public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("Betsy");
+        names.add("Anita");
+        names.add("Murshed");
+        names.add("Hamish");
+        System.out.println(names);
+    }
+}
---
+++ arrays3.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class arrays3 {
+    public static void main(String[] args){
+        ArrayList<String> names = new ArrayList<String>();
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+            names.add(args[i]);
+        }
+        System.out.println(names);
+    }
+}
