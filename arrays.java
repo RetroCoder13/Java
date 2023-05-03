d03f4b8d7146bf0ef2eaa1d71b018ce7e954a5e8
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-05-03 10:56:30 +0000

    More arrays!

--- arrays.java
+++ arrays.java
@@ -1,6 +1,7 @@
 public class arrays {
     public static void main(String[] args){
         String[] names = {"Alf","Betsy","Charlie","David"};
+        names[2] = "Bob";
         System.out.println(names[2]);
     }
 }
