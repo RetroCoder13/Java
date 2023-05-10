30a80a6308f5f35273474a09e52f1ad0cc2cddb4
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-05-10 11:13:04 +0000

    File Handling in Java

--- FileHandling/file.java
+++ FileHandling/file.java
@@ -2,16 +2,21 @@ package FileHandling;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.FileReader;
+import java.util.Scanner;
 import java.io.IOException;
 
 public class file {
     public static void main(String[] args){
         try{
             File file = new File("./FileHandling/example.txt");
-            FileWriter fileWriter = new FileWriter("./FileHandling/example.txt");
-            fileWriter.write("I wrote to a file!");
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write("I wrote to a file!!");
             fileWriter.close();
+            Scanner fileReader = new Scanner(file);
+            while (fileReader.hasNextLine()) {
+                System.out.println(fileReader.nextLine());
+            }
+            fileReader.close();
         }
         catch(IOException e) {}
     }
