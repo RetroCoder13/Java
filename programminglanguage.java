de1ae250ffe2e97bdf864e0c163e7a2c9b0da63b
RetroCoder <79114787+RetroCoder13@users.noreply.github.com>
2023-11-16 15:37:35 +0000

    Added variables

--- programminglanguage.java
+++ programminglanguage.java
@@ -9,25 +9,28 @@ public class programminglanguage {
             input = System.console().readLine().split(" ");
             if(input[0].equals("OUT")){
                 if(input[1].equals("ADD") || input[1].equals("SUB") || input[1].equals("MUL") || input[1].equals("DIV") || input[1].equals("POW")){
-                    function_out(Double.toString(function_numbers(Double.parseDouble(input[2]), Double.parseDouble(input[3]),input[1])));
+                    function_out(Double.toString(function_numbers(input[2], input[3], input[1], variables)), variables);
                 } else {
                     for(int i=0;i<input.length-1;i++){
-                        function_out(input[i+1]);
-                        function_out(" ");
+                        function_out(input[i+1], variables);
+                        function_out(" ", variables);
                     }
                 }
-                function_out("\n");
+                function_out("\n", variables);
             }
 
             if(input[0].equals("VAR")){
                 function_var(input[1], input[2], variables);
-                System.out.println(variables);
             }
         }
     }
 
-    public static void function_out(String output){
-        System.out.print(output);
+    public static void function_out(String output, HashMap<String,String> variables){
+        if(variables.keySet().contains(output)){
+            System.out.print(variables.get(output));
+        } else {
+            System.out.print(output);
+        }
     }
 
     public static String function_in(){
@@ -39,17 +42,55 @@ public class programminglanguage {
     }
 
 
-    public static double function_numbers(double a, double b, String operation){
-        if(operation.equals("ADD")){
-            return function_add(a, b);
-        } else if(operation.equals("SUB")){
-            return function_sub(a, b);
-        } else if(operation.equals("MUL")){
-            return function_mul(a, b);
-        } else if(operation.equals("DIV")){
-            return function_div(a, b);
+    public static double function_numbers(String a, String b, String operation, HashMap<String,String> variables){
+        if(variables.keySet().contains(a) && variables.keySet().contains(b)){
+if(operation.equals("ADD")){
+                return function_add(Double.parseDouble(variables.get(a)), Double.parseDouble(variables.get(b)));
+            } else if(operation.equals("SUB")){
+                return function_sub(Double.parseDouble(variables.get(a)), Double.parseDouble(variables.get(b)));
+            } else if(operation.equals("MUL")){
+                return function_mul(Double.parseDouble(variables.get(a)), Double.parseDouble(variables.get(b)));
+            } else if(operation.equals("DIV")){
+                return function_div(Double.parseDouble(variables.get(a)), Double.parseDouble(variables.get(b)));
+            } else {
+                return function_pow(Double.parseDouble(variables.get(a)), Double.parseDouble(variables.get(b)));
+            }
+        } else if(variables.keySet().contains(a)){
+            if(operation.equals("ADD")){
+                return function_add(Double.parseDouble(variables.get(a)), Double.parseDouble(b));
+            } else if(operation.equals("SUB")){
+                return function_sub(Double.parseDouble(variables.get(a)), Double.parseDouble(b));
+            } else if(operation.equals("MUL")){
+                return function_mul(Double.parseDouble(variables.get(a)), Double.parseDouble(b));
+            } else if(operation.equals("DIV")){
+                return function_div(Double.parseDouble(variables.get(a)), Double.parseDouble(b));
+            } else {
+                return function_pow(Double.parseDouble(variables.get(a)), Double.parseDouble(b));
+            }
+        } else if(variables.keySet().contains(b)){
+            if(operation.equals("ADD")){
+                return function_add(Double.parseDouble(a), Double.parseDouble(variables.get(b)));
+            } else if(operation.equals("SUB")){
+                return function_sub(Double.parseDouble(a), Double.parseDouble(variables.get(b)));
+            } else if(operation.equals("MUL")){
+                return function_mul(Double.parseDouble(a), Double.parseDouble(variables.get(b)));
+            } else if(operation.equals("DIV")){
+                return function_div(Double.parseDouble(a), Double.parseDouble(variables.get(b)));
+            } else {
+                return function_pow(Double.parseDouble(a), Double.parseDouble(variables.get(b)));
+            }
         } else {
-            return function_pow(a, b);
+            if(operation.equals("ADD")){
+                return function_add(Double.parseDouble(a), Double.parseDouble(b));
+            } else if(operation.equals("SUB")){
+                return function_sub(Double.parseDouble(a), Double.parseDouble(b));
+            } else if(operation.equals("MUL")){
+                return function_mul(Double.parseDouble(a), Double.parseDouble(b));
+            } else if(operation.equals("DIV")){
+                return function_div(Double.parseDouble(a), Double.parseDouble(b));
+            } else {
+                return function_pow(Double.parseDouble(a), Double.parseDouble(b));
+            }
         }
     }
     public static double function_add(double a, double b){
